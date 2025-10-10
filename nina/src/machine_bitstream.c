139745f8c378c3b40da8ce99119c88b713b725e9
Antonio Galea <antonio.galea@gmail.com>
2025-10-10 19:24:42 +0200

    bitstream: use DWT instead of SysTick, and access GPIO directly (WIP)

--- nina/src/machine_bitstream.c
+++ nina/src/machine_bitstream.c
@@ -4,36 +4,62 @@
 #include "py/mphal.h"
 #include <stdio.h>
 
-#include <nrfx_systick.h>
-#define MP_HAL_BITSTREAM_TICKS_OVERHEAD  (10)
+#define NRF_GPIO_PIN_MAP(port, pin) (((pin) & 0x1F) | ((port) << 5))
+
+#define TICKS_PER_US    64
 
 void machine_bitstream_high_low(mp_hal_pin_obj_t pin, uint32_t *timing_ns, const uint8_t *buf, size_t len) {
-    uint32_t us_to_ticks = SystemCoreClock / 1000000;
     // Convert ns to clock ticks [high_time_0, period_0, high_time_1, period_1].
     for (size_t i = 0; i < 4; ++i) {
-        timing_ns[i] = timing_ns[i] * us_to_ticks / 1000;
-        if (timing_ns[i] > (2 * MP_HAL_BITSTREAM_TICKS_OVERHEAD)) {
-            timing_ns[i] -= MP_HAL_BITSTREAM_TICKS_OVERHEAD;
-        }
+        timing_ns[i] = timing_ns[i] * TICKS_PER_US / 1000;
         if (i % 2 == 1) {
             // Convert low_time to period (i.e. add high_time).
-            timing_ns[i] += timing_ns[i - 1] - MP_HAL_BITSTREAM_TICKS_OVERHEAD;
+            timing_ns[i] += timing_ns[i - 1];
         }
     }
 
     mp_hal_pin_output(pin);
-    for (size_t i = 0; i < len; ++i) {
-        uint8_t b = buf[i];
-        for (size_t j = 0; j < 8; ++j) {
-            uint8_t bit = b >> 6 & 2;
-            uint32_t *t = &timing_ns[bit];
-            uint32_t start = nrf_systick_val_get();
-            mp_hal_pin_write(pin, 1);
-            while ((NRF_SYSTICK_VAL_MASK & (start - nrf_systick_val_get())) < t[0]);
-            b <<= 1;
-            mp_hal_pin_write(pin, 0);
-            while ((NRF_SYSTICK_VAL_MASK & (start - nrf_systick_val_get())) < t[1]);
+
+    uint8_t port_num = 0; // TODO
+    NRF_GPIO_Type *reg = NRF_GPIO_PIN_MAP(port_num, pin->pin);
+    uint32_t mask = (1UL << pin->pin);
+
+    // Enable DWT in debug core
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
+    while (1) {
+        uint32_t start = DWT->CYCCNT;
+        uint32_t cycles = 0;
+        uint32_t total_cycles = 0;
+
+        for (size_t i = 0; i < len; ++i) {
+            uint8_t b = buf[i];
+            for (size_t j = 0; j < 8; ++j) {
+                uint8_t bit = b >> 6 & 2;
+                uint32_t *t = &timing_ns[bit];
+                cycles = DWT->CYCCNT;
+                reg->OUTSET |= mask;
+                while (DWT->CYCCNT - cycles < t[0]) {
+                        ;
+                }
+                reg->OUTCLR |= mask;
+                while (DWT->CYCCNT - cycles < t[1]) {
+                        ;
+                }
+                total_cycles += t[1];
+            }
         }
+
+        // If total time longer than 25%, resend the whole data.
+        // Since we are likely to be interrupted by SoftDevice
+        cycles = DWT->CYCCNT - start;
+        if (cycles < (total_cycles * 5) / 4) {
+            break;
+        }
+printf("WARNING: resend! cycles=%u, total_cycles=%u\n", cycles, total_cycles);
+        // re-send need 300us delay
+        mp_hal_delay_us(300);
     }
 }
 
